b8721ba73a720dac5654edcfaf03fcd1a90eb9b2
ameysunu <ameysunu00719@gmail.com>
2021-11-15 11:52:54 +0000

    hour and minute hand added

--- AnimationClock/app/src/main/java/com/example/animationclock/ClockSurfaceView.java
+++ AnimationClock/app/src/main/java/com/example/animationclock/ClockSurfaceView.java
@@ -7,6 +7,10 @@ import android.graphics.Paint;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
 public class ClockSurfaceView extends SurfaceView implements Runnable {
     private float length;
     private Thread thread;
@@ -43,6 +47,8 @@ public class ClockSurfaceView extends SurfaceView implements Runnable {
     @Override
     public void run() {
         int sec = 0;
+        int min = 0;
+        int hour = 0;
         while (running) {
             if (holder.getSurface().isValid()) {
                 //get canvas
@@ -55,24 +61,34 @@ public class ClockSurfaceView extends SurfaceView implements Runnable {
 
                 //draw the marks
                 RegPoly secMarks = new RegPoly(60, getWidth()/2, getHeight()/2, this.length, canvas, forePaint);
-                RegPoly hourMarks = new RegPoly(60, getWidth()/2, getHeight()/2, this.length-10, canvas, forePaint);
+                RegPoly hourMarks = new RegPoly(12, getWidth()/2, getHeight()/2, this.length-10, canvas, forePaint);
                 secMarks.drawNodes();
                 hourMarks.drawNodes();
 
                 //make 3 regpoly-s for the hand
                 RegPoly secHand = new RegPoly(60, getWidth()/2, getHeight()/2, this.length-20, canvas, forePaint);
+                RegPoly hourHand = new RegPoly(1, getWidth()/2, getHeight()/2, this.length-80, canvas, forePaint);
+                RegPoly minHand = new RegPoly(60, getWidth()/2, getHeight()/2, this.length-40, canvas, forePaint);
 
                 //get hour, min, sec from calendar
+                Calendar calendar = Calendar.getInstance();
+                hour = calendar.get(Calendar.HOUR);
+                min = calendar.get(Calendar.MINUTE);
+                sec = calendar.get(Calendar.SECOND);
+
 
                 // draw the 3 hands
                 secHand.drawRadius(sec);
+                minHand.drawRadius(min);
+                hourHand.drawRadius(hour);
+
 
                 try {
                     Thread.sleep(1000);
                 } catch(Exception e) {
 
                 }
-                sec++;
+                //sec++;
 
                 holder.unlockCanvasAndPost(canvas);
             }
